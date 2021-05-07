3dfee05ac37d19c7e704c471f9500edbc0174693
Isaac <isaac.ewing3@gmail.com>
2021-05-07 15:43:26 -0600

    made eslint stop complaining, refactored the getRandomThrow function

--- utils.js
+++ utils.js
@@ -1,27 +1,28 @@
 export { didUserWin, getRandomThrow };
 
 function didUserWin(userThrow, computerThrow) {
-   if (
-       (userThrow === 'rock' && computerThrow === 'scissors')
+    if (
+        (userThrow === 'rock' && computerThrow === 'scissors')
        || (userThrow === 'scissors' && computerThrow === 'paper')
        || (userThrow === 'paper' && computerThrow === 'rock')
-   ) {
-       return 'user';
-   } else if (userThrow === computerThrow) {
-       return 'draw';
-   } else {
-       return 'computer';
-   }  
+    ) {
+        return 'user';
+    } else if (userThrow === computerThrow) {
+        return 'draw';
+    } else {
+        return 'computer';
+    }  
 }
 
-function getRandomThrow(computerChoice) {
+function getRandomThrow() {
+    const computerNumber = Math.ceil(Math.random() * 3);
 
-    if(computerChoice === 1)
+    if (computerNumber === 1)
         return 'rock';
     
-    if(computerChoice === 2)
+    if (computerNumber === 2)
         return 'paper';
     
-    if(computerChoice === 3)
+    if (computerNumber === 3)
         return 'scissors';
 }
